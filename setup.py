d9e91d40d73221736529d82f918c3c71b3d5e818
Adam Pridgen <adam.pridgen@thecoverofnight.com>
2017-09-22 23:25:08 -0500

    adding missing package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='slow-hitter',
       description=DESC,
       author='adam pridgen',
       author_email='dso@thecoverofnight.com',
-      install_requires=['kombu', ],
+      install_requires=['kombu', 'redis'],
       packages=find_packages('src'),
       package_dir={'': 'src'},
 )
